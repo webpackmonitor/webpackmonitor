1548151f095bb16d998fe5b6f119de674a780f42
Balal Zuhair <balalzuhair@Balals-MacBook-Pro.local>
2017-09-16 20:22:29 -0700

    add jquery

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import $ from 'jquery';
 
 const Dashboard = (props) => {
   const id = props.match.params.buildid;
+
+
   return (
-    <h1>Dashboard for {id}</h1>
+    <h1 id="dash">Dashboard for {id}</h1>
   );
 };
 
