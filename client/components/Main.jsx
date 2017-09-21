378254d12acd173f682860d5aa1604f31380e813
Balal Zuhair <balalzuhair@Balals-MacBook-Pro.local>
2017-09-21 10:10:25 -0700

    changed pure function to component

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -4,16 +4,26 @@ import BuildRoutes from './BuildComponents/BuildRoutes';
 import Overview from './Overview';
 import Recommendations from './Recommendations/Recommendations';
 import Performance from './Performance';
+import build from './../.././monitor/stats.json';
 
-const Main = () => (
-  <main>
-    <Switch>
-      <Route exact path="/" component={Overview} />
-      <Route path="/builds" component={BuildRoutes} />
-      <Route path="/performance" component={Performance} />
-      <Route path="/recommendations" component={Recommendations} />
-    </Switch>
-  </main>
-);
+class Main extends React.Component {
+  constructor() {
+    super()
+    this.state = { build }
+  }
+  render() {
+    console.log(this.state.build)
+    return  (
+      <main>
+        <Switch>
+          <Route exact path="/" component={Overview} />
+          <Route path="/builds" component={BuildRoutes} />
+          <Route path="/performance" component={Performance} />
+          <Route path="/recommendations" component={Recommendations} />
+        </Switch>
+      </main>
+    );
+  }
+}
 
 export default Main;
