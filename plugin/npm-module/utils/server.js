bb257c046cb9c54f9ccf58cf5d9155c9ad8e1418
mbehzad <m_online_net@yahoo.com>
2017-12-01 19:26:08 +0100

    watch mode: adds console warning when the express server is already running and starting a new one on the same port throws a error.

--- plugin/npm-module/utils/server.js
+++ plugin/npm-module/utils/server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const opener = require('opener');
 const colors = require('colors');
 
+const PORT_IN_USE_ERROR_CODE = "EADDRINUSE";
+
 module.exports = (data, port, update) => {
   const app = express();
   const url = `http://localhost:${port}/`;
@@ -25,5 +27,20 @@ module.exports = (data, port, update) => {
     opener(url);
     console.log(colors.bold('\nWebpack-Monitor'), `is running on port ${port}!`);
     console.log(colors.italic.red('Press ctrl C to exit'));
+  })
+  .on('error', err => {
+    if (err.code === PORT_IN_USE_ERROR_CODE) {
+        console.log("\n");
+        console.log(colors.italic.red("webpack-monitor couldn't be launched:", `Port ${port} is currently in use.`));
+        console.log(colors.red("Make sure webpack-monitor is only running once."));
+        console.log(colors.gray("see: "), colors.cyan("https://github.com/webpackmonitor/webpackmonitor/issues/48"));
+        console.log(colors.gray("Error:"));
+        console.error(err);
+        console.log("\n", colors.gray("This would not effect the webpack build, nor the webpack-monitor capturing the stats."), "\n");
+    }
+    // error was not a "port already in use". Don't swallow the error.
+    else {
+      throw err;
+    }
   });
 };
